7007f1167323ba30f5c1b0426db65ef07245c136
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-14 16:05:16 +0200

    refactor: rename project

--- engage_sante/asgi.py
+++ engage_sante/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for engage_sante_project project.
+ASGI config for engage_sante project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engage_sante_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engage_sante.settings')
 
 application = get_asgi_application()
--- engage_sante/wsgi.py
+++ engage_sante/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for engage_sante_project project.
+WSGI config for engage_sante project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engage_sante_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engage_sante.settings')
 
 application = get_wsgi_application()
