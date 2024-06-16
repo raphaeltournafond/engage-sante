84d4bbf5ff663c843ca4d9639e7c813b84da9719
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-16 22:13:11 +0200

    feat: ajout du login

--- patients/urls.py
+++ patients/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('patients/', views.list_patients, name='list_patients'),
     path('modifier/<int:user_id>/', views.update_utilisateur, name='update_utilisateur'),
     path('supprimer/<int:user_id>/', views.delete_utilisateur, name='delete_utilisateur'),
+    path("connexion/", auth_views.LoginView.as_view(template_name="patients/login.html"), name='login'),
     path('enregistrer/patient/', views.register_patient, name='register_patient'),
     path('enregistrer/medecin/', views.register_medecin, name='register_medecin'),
+    path("reset/", auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path("reset-done/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 ]
