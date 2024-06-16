d329cc74480f9f624f547a35f6fc84681b9c1a34
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-17 01:31:45 +0200

    feat: add all permissions

--- patients/views.py
+++ patients/views.py
@@ -9,34 +9,41 @@ from .models import Utilisateur
 
 @login_required
 def list_patients(request):
-    # Filtre les utilisateurs qui ne sont pas médecins
-    patients = Utilisateur.objects.filter(is_medecin=False, is_staff=False, is_superuser=False)
-    return render(request, 'patients/list.html', {'patients': patients})
+    if request.user.is_staff:
+        patients = Utilisateur.objects.filter(is_medecin=False, is_staff=False, is_superuser=False)
+        return render(request, 'patients/list.html', {'patients': patients})
+    return redirect('not_authorized')
 
 @login_required
 def info_utilisateur(request, user_id):
     utilisateur = get_object_or_404(Utilisateur, id=user_id)
-    return render(request, 'patients/info.html', {'utilisateur': utilisateur})
+    if request.user.is_staff or request.user.id == utilisateur.id:
+        return render(request, 'patients/info.html', {'utilisateur': utilisateur})
+    return redirect('not_authorized')
 
 @login_required
 def update_utilisateur(request, user_id):
-    patient = get_object_or_404(Utilisateur, id=user_id)
-    if request.method == 'POST':
-        form = UtilisateurUpdateForm(request.POST, instance=patient)
-        if form.is_valid():
-            form.save()
-            return redirect('list_patients')
-    else:
-        form = UtilisateurUpdateForm(instance=patient)
-    return render(request, 'patients/update.html', {'form': form, 'patient': patient})
+    utilisateur = get_object_or_404(Utilisateur, id=user_id)
+    if request.user.is_staff or request.user.id == utilisateur.id:
+        if request.method == 'POST':
+            form = UtilisateurUpdateForm(request.POST, instance=utilisateur)
+            if form.is_valid():
+                form.save()
+                return redirect('list_patients')
+        else:
+            form = UtilisateurUpdateForm(instance=utilisateur)
+        return render(request, 'patients/update.html', {'form': form, 'utilisateur': utilisateur})
+    return redirect('not_authorized')
 
 @login_required
 def delete_utilisateur(request, user_id):
-    patient = get_object_or_404(Utilisateur, id=user_id)
-    if request.method == 'POST':
-        patient.delete()
-        return redirect('list_patients')
-    return render(request, 'patients/delete.html', {'patient': patient})
+    utilisateur = get_object_or_404(Utilisateur, id=user_id)
+    if request.user.is_staff or request.user.id == utilisateur.id:
+        if request.method == 'POST':
+            utilisateur.delete()
+            return redirect('list_patients')
+        return render(request, 'patients/delete.html', {'utilisateur': utilisateur})
+    return redirect('not_authorized')
 
 class CustomLoginView(LoginView):
     template_name = 'patients/login.html'
@@ -65,15 +72,17 @@ def register_patient(request):
 
 @login_required
 def register_medecin(request):
-    if request.method == 'POST':
-        form = MedecinCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('list_patients')
-    else:
-        form = MedecinCreationForm()
-    return render(request, 'patients/register.html', {'form': form})
+    if request.user.is_staff:
+        if request.method == 'POST':
+            form = MedecinCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect('list_patients')
+        else:
+            form = MedecinCreationForm()
+        return render(request, 'patients/register.html', {'form': form})
+    return redirect('not_authorized')
 
 def not_authorized(request):
     return render(request, 'patients/not_authorized.html')
