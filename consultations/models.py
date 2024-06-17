27c121672653cb506a7f5b9328886bcd6f3adeff
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-17 14:11:26 +0200

    feat: ajout du modèle consultation

--- consultations/models.py
+++ consultations/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from patients.models import Utilisateur
 
-# Create your models here.
+class Consultation(models.Model):
+    TYPE_CHOICES = [
+        ('Visite', 'Visite'),
+        ('Suivi', 'Suivi'),
+        ('Operation', 'Opération'),
+    ]
+
+    patient = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, related_name='consultations')
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.nom} - {self.patient.first_name} {self.patient.last_name}"
--- engage_sante/settings.py
+++ engage_sante/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'patients',
+    'consultations',
 ]
 
 MIDDLEWARE = [
