2c703526f4f3f5f012b38611d7503c27ded38877
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-18 15:02:28 +0200

    feat: ajout commande création médecin

---
+++ patients/management/commands/medecin.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from patients.models import Utilisateur as Medecin
+from patients.models import generate_unique_username
+import getpass
+
+fake = Faker('fr_FR')
+
+class Command(BaseCommand):
+    help = "Créé un médecin"
+
+    def handle(self, *args, **options):
+        last_name = input("Entrer le nom de famille : ")
+        first_name = input("Entrer le prénom : ")
+        password = None
+        password_confirm = None
+        while not password or not password_confirm or (password != password_confirm):
+            if password and password_confirm:
+                self.stdout.write(
+                    self.style.ERROR('Les mots de passe sont différents')
+                )
+            password = getpass.getpass(prompt="Mot de passe : ")
+            password_confirm = getpass.getpass(prompt="Confirmer le mot de passe : ")
+        username = generate_unique_username(last_name, first_name)
+        medecin = Medecin.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=fake.email(),
+            rue=fake.street_address(),
+            cp=fake.postcode(),
+            ville=fake.city(),
+            is_medecin=True,
+            is_superuser=True,
+            is_active=True,
+            is_staff=True,
+        )
+
+        self.stdout.write(
+            self.style.SUCCESS('Le medecin a bien été créé, identifiant unique : "%s".' % username)
+        )
