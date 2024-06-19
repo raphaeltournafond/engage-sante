a5fef0fccfea42ca1cc478deec8b0743158140ee
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-19 22:43:11 +0200

    wip: update settings for tailwind

--- engage_sante/settings.py
+++ engage_sante/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
+    'theme',
     'patients',
     'consultations',
 ]
@@ -152,3 +153,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Tailwind
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
