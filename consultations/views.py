ab111a3e85fb51d1877ced9d253fe1dacd02d9eb
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-21 15:41:16 +0200

    feat: add htmx

--- consultations/views.py
+++ consultations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from consultations.forms import ConsultationForm
 from patients.models import Utilisateur
 from .models import Consultation
@@ -43,10 +45,13 @@ def update_consultation(request, id):
     return redirect('not_authorized')
 
 @login_required
+@require_http_methods(['DELETE'])
 def delete_consultation(request, id):
+    print('Test')
     if request.user.is_staff:
         consultation = get_object_or_404(Consultation, id=id)
-        if request.method == 'POST':
-            consultation.delete()
-            return redirect('update_utilisateur', user_id=consultation.patient.id)
-    return redirect('not_authorized')
+        consultation.delete()
+        response = HttpResponse(status=204)
+        response['HX-Trigger'] = 'delete-consultation'
+        return response
+    return HttpResponse({'error': 'Vous ne pouvez pas supprimer cette consultation.'}, status=403)
