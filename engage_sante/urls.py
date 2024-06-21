847b4687d6e3e368b660f48749e61f5790d2f728
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-06-21 09:22:35 +0200

    feat: add mentions

--- engage_sante/urls.py
+++ engage_sante/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from .views import home
+from .views import home, mentions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('mentions/', mentions, name='mentions'),
     path('', include('patients.urls')),
     path('', include('consultations.urls')),
 ]
--- engage_sante/views.py
+++ engage_sante/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def mentions(request):
+    return render(request, 'mentions.html')
